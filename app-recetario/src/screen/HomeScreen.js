1f71d0258ffcd34f4652202f0d1c3398e504b254
robermancera <48019891+robermancera@users.noreply.github.com>
2022-05-24 17:27:30 -0500

    Uptading to recent
    Co-authored-by: nicolasjrz <nicolasjrz.dev@gmail.com>

--- app-recetario/src/screen/HomeScreen.js
+++ app-recetario/src/screen/HomeScreen.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import { StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View, TouchableHighlight ,FlatList, Image} from 'react-native';
 import Search from '../components/Search';
 import Carousel from '../components/Carousel';
 
 import Recetario from '../data/recetario.json'
 
-export default function HomeScreen() {
+import ViewReceta from './ViewReceta';
+
+
+
+function isRecent(){
+
+  let arr = [1,2,3]
+
+  let array = [];
+      //console.log(Recetario);
+      Recetario.map(function(index,id){
+
+        for (let i = 0; i < arr.length; i++) {
+            if(id==arr[i]){
+              array.push(index);
+            }
+          
+        }
+
+       
+      })
+     console.log(array); 
+
+     return array;
+}
+
+
+
+export default function HomeScreen({navigation}) {
+  let arrayRecent= isRecent();
   return (
    <View style={styles.container}>
       <Search/> 
        <Carousel title="Trending" data={Recetario} />
-       <Carousel title="Recent"/> 
+       <Carousel title="Recent" data={arrayRecent}/> 
    </View> 
+   
    
   );
 };
