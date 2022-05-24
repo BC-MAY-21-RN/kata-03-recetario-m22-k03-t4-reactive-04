1ecc630495fdccb21fc02eba04b3f09843bf83fd
robermancera <48019891+robermancera@users.noreply.github.com>
2022-05-24 17:26:42 -0500

    Updating to recent
    Co-authored-by: nicolasjrz <nicolasjrz.dev@gmail.com>

--- app-recetario/src/screen/ViewReceta.js
+++ app-recetario/src/screen/ViewReceta.js
@@ -1,18 +1,28 @@
 import React from 'react';
-import {View,StyleSheet} from 'react-native';
-import {useRoute} from '@react-navigation/native';
+import {Text,View,ImageBackground,FlatList, StyleSheet} from 'react-native';
+import { useNavigation ,useRoute} from '@react-navigation/native';
+import ListIngredients from '../components/ListIngredients';
+
+import TouchBar from '../components/TouchBar';
 import RecetaHeader from '../components/RecetaHeader';
 import RecetaBody from '../components/RecetaBody';
+import { Button } from 'react-native-elements';
+
+
 
 export default function ViewReceta() {
-    const route = useRoute()
-   // console.log(route.params.item)
 
+  let array = [];
+
+    const route = useRoute()
+   ///console.log(route.params.item)
+    array.push(route.params.item.id)
+    console.log(array)
     ///const {image , name } = route.params.item;
     return (
         <View style={styles.container}>  
             <RecetaHeader style={styles.header} title={route.params.item.name} imagen={route.params.item.image } />
-            <RecetaBody style={styles.body} data={route.params.item.ingredients}  />
+            <RecetaBody style={styles.body} data={route.params.item.ingredients}/>
         </View>
     );
 }
