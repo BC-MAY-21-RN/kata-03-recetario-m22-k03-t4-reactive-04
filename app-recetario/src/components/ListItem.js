f92142cdcc3660b7d752d42e636b70c12c2a28f3
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-07-22 22:32:54 -0300

    add component list item

--- app-recetario/src/components/ListItem.js
+++ app-recetario/src/components/ListItem.js
@@ -1,16 +1,18 @@
 import React from "react";
-import {StyleSheet, TouchableHighlight } from "react-native";
-import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, TouchableHighlight } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import Item from "../components/Item";
 
 const ListItem = ({ item }) => {
   const navigation = useNavigation();
 
   return (
- 
-      <TouchableHighlight onPress={() => navigation.navigate("ViewReceta",{item})} style={styles.container}>
-        <Item item={item}/>
-      </TouchableHighlight>
+    <TouchableHighlight
+      onPress={() => navigation.navigate("ViewReceta", { item })}
+      style={styles.container}
+    >
+      <Item item={item} />
+    </TouchableHighlight>
   );
 };
 
@@ -18,15 +20,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
-    width:160,
-    height:170,
-    alignItems:"center",
-    backgroundColor:"transparent",     
-  }
- 
+    width: 160,
+    height: 170,
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
 });
 
-
-
 export default ListItem;
-
